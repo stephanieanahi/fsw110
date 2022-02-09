6d3f9d2491af18dda8a04eb817055a48d461bd07
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-02-09 16:10:25 -0600

    linked js file tohtml file and created a list

---
+++ week2/index.js
@@ -0,0 +1,14 @@
+var h=document.createElement("h1")
+var t = document.createTextNode("Welcome to my JS site")
+h.appendChild(t)
+document.body.appendChild(h)
+
+
+var newP= document.createElement('p')
+newP.textContent='All of this was created with javascript'
+document.body.appendChild(newP)
+
+var newListItem=document.createElement(li)
+var myList= document.getElementById('my-List')
+document.body.appendChild(mylist)
+console.log(document)
